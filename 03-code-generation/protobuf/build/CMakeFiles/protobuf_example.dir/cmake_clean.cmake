998743ed3286832db75669a4d23f2c76b3f809d2
alexook <gilber00774@gmail.com>
2023-01-10 17:53:00 +0800

    New commit

---
+++ 03-code-generation/protobuf/build/CMakeFiles/protobuf_example.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "AddressBook.pb.cc"
+  "AddressBook.pb.h"
+  "CMakeFiles/protobuf_example.dir/AddressBook.pb.cc.o"
+  "CMakeFiles/protobuf_example.dir/main.cpp.o"
+  "protobuf_example"
+  "protobuf_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/protobuf_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
